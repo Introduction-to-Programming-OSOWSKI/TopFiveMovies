a79408044b83f47ceb01d2509b5a9ecb819796a9
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-11-24 20:03:30 +0000

    Added Due Date

--- src/test/java/assignment/Tests.java
+++ src/test/java/assignment/Tests.java
@@ -1,6 +1,7 @@
 package assignment;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,9 +15,16 @@ public class Tests {
 
 
    @Test
-   public void testTopFiveMovies(){
+   public void code(){
        App.main(null);
        assertEquals("a, b, c, e, f", App.topFiveMovies("a", "b", "c", "d", "e"), "INCORRECT. DID OYU ADD COMMAS AND SPACES?");
    }
+
+   @Test
+    public void late() {
+        App.main(null);
+        assertTrue(DueDate.onTime(2020, 12, 16), "Submitted Late");
+    }
+
 
 }
